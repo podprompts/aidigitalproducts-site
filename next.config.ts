996cc8f42a588b00f186437c28f61ffa0c857f0b
podprompts <adrien1@gmail.com>
2026-07-20 06:03:06 -0700

    Update CDN domain to cdn.hireaireceptionist.com

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "cdn.allnaturalbox.com",
+        hostname: "cdn.hireaireceptionist.com",
         pathname: "/**",
       },
     ],
